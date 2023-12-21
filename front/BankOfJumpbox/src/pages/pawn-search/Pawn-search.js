151bee231abdc0e33da7b79d6df1a5302c25d5f6
Anon Jansaichon <a.jansaichon@gmail.com>
2023-12-21 23:24:42 +0700

    add data

--- front/BankOfJumpbox/src/pages/pawn-search/Pawn-search.js
+++ front/BankOfJumpbox/src/pages/pawn-search/Pawn-search.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Input } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
+
 
 const PawnSearch = () => {
+    const [data,setData] = useState([])
     const handleClick = () => {
-        console.log('gg')
+        axios.get('https://tcc-01.th1.proen.cloud/bojpawndevback/api/CollateralsTx')
+            .then(function (res) {
+                console.log('res', res.data.data)
+                setData(res.data.data)
+            }).then((res) => {
+                console.log('hhh',data)
+            })
     }
 
     return (
@@ -14,6 +23,28 @@ const PawnSearch = () => {
             <Button variant="contained" color="primary" onClick={() => handleClick()}>
                 submit
             </Button>
+            <div>
+                data
+                {
+                    data.map((item) => {
+                        return(
+                            <ul>
+                                <li>{item.store}</li>
+                                <li>Status : {item.statusCode}</li>
+                            </ul>
+                        )
+                        {/* item.collateralDetaills.map((item2) =>{
+                            return(
+                                <ul>
+                                    <li>
+                                        {item2.collateralItemName}
+                                    </li>
+                                </ul>
+                            )
+                        }) */}
+                    })
+                }
+            </div>
         </view>
     )
 }
